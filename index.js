71ee673bab3412c07328305ff1f874c49ef19d3e
chieffancypants <chieffancypants@gmail.com>
2013-08-27 18:44:33 -0400

    Get this working with a more native solution (for linux)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var util = require('util');
-var growly = require('growly');
+var growl = require('growl');
 var path = require('path');
 
 var MSG_SUCCESS = '%d tests passed in %s.';
@@ -8,19 +8,16 @@ var MSG_ERROR = '';
 
 var OPTIONS = {
   success: {
-    dispname: 'Success',
     title: 'PASSED - %s',
-    icon: path.join(__dirname, 'images/success.png')
+    image: path.join(__dirname, 'images/success.png')
   },
   failed: {
-    dispname: 'Failure',
     title: 'FAILED - %s',
-    icon: path.join(__dirname, 'images/failed.png')
+    image: path.join(__dirname, 'images/failed.png')
   },
   error: {
-    dispname: 'Aborted',
     title: 'ERROR - %s',
-    icon: path.join(__dirname, 'images/error.png')
+    image: path.join(__dirname, 'images/error.png')
   }
 };
 
@@ -33,15 +30,6 @@ var GrowlReporter = function(helper, logger, config) {
     return helper.merge(OPTIONS[type], {title: prefix + util.format(OPTIONS[type].title, browser)});
   };
 
-  growly.register('Karma', '', [], function(error) {
-    var warning = 'No running version of GNTP found.\n' +
-	                'Make sure the Growl service is installed and running.\n' +
-                  'For more information see https://github.com/theabraham/growly.';
-    if (error) {
-      log.warn(warning);
-    }
-  });
-
   this.adapters = [];
 
   this.onBrowserComplete = function(browser) {
@@ -49,15 +37,15 @@ var GrowlReporter = function(helper, logger, config) {
     var time = helper.formatTimeInterval(results.totalTime);
 
     if (results.disconnected || results.error) {
-      return growly.notify(MSG_ERROR, optionsFor('error', browser.name));
+      return growl(MSG_ERROR, optionsFor('error', browser.name));
     }
 
     if (results.failed) {
-      return growly.notify(util.format(MSG_FAILURE, results.failed, results.total, time),
+      return growl(util.format(MSG_FAILURE, results.failed, results.total, time),
           optionsFor('failed', browser.name));
     }
 
-    growly.notify(util.format(MSG_SUCCESS, results.success, time), optionsFor('success',
+    growl(util.format(MSG_SUCCESS, results.success, time), optionsFor('success',
         browser.name));
   };
 };
